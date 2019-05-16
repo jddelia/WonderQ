58872739d4d7e2fb3d03c33f52f646f7df23bf81
jddelia <jddelia123@gmail.com>
2019-05-16 18:42:39 -0400

    Refactor createQueue into module

--- routes/router.js
+++ routes/router.js
@@ -1,37 +1,10 @@
 const express = require("express");
-const consumer = require('../consumer');
+const createQueue = require('../queue');
 
 const router = express.Router();
 
 // Schema
 const QueueItems = require('../models/Queue');
-
-// Queue
-let queue = [];
-
-async function createQueue() {
-  const items = await QueueItems.find({ active: false });
-  queue = [...items];
-
-  const processingInterval = setInterval(async () => {
-
-    // process all messages
-    while (queue.length > 0) {
-      let job = queue.shift();
-
-      // Process message from queue
-      let restoredItem = await consumer.process(QueueItems, job);
-
-      // Return item to queue if restored
-      restoredItem ? queue.push(restoredItem) : null;
-    }
-
-    if (queue.length <= 0) {
-      console.log('DONE')
-      clearInterval(processingInterval);
-    }
-  }, 10000);
-}
 
 // Routes
 // Get all items in queue
